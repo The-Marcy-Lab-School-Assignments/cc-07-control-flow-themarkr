4f91db6f6a7e30d055325696aea9c57c122dad90
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-10-13 14:36:51 +0000

    completed all three problems

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 //1
 function sumOfFourAndSix(){
-  
+  let multiplesSum = 0;
+  for (let i = 1 ; i<=1000; i++){
+      if(i % 4 === 0 && i % 6 ===0){
+          multiplesSum += i;
+      }
+  }
+  return multiplesSum;
 }
 
 //2
 function oddAndEvenInFifteen(){
-  
+  for (let i = 0; i <= 15; i++){
+      if( i % 2 === 0){
+          console.log(`${i} is even`);
+      }else{
+          console.log(`${i} is odd`);
+      }
+  }
 }
 
 //3
-function assignGrade(){
-  
+function assignGrade(num){
+  if (num <= 100){
+      if(num >=91){
+         return "A";
+      }else if(num >=81){
+          return "B";
+      }else if(num >= 71){
+          return "C";
+      }else if(num >= 65){
+          return "D";
+      }else{
+          return "F";
+      }
+  }
 }
+//carmen said to try doing it without &&
+//   }else if(num <= 90 && num >=81){
+//       return "B";
+//   }else if(num <= 80 && num >=71){
+//       return "C";
+//   }else if(num <= 70 && num >=65){
+//       return "B";
+//   }else{
+//       return "F";
+//   }
+// }
 
-
-
+console.log(sumOfFourAndSix());
+oddAndEvenInFifteen();
+console.log(assignGrade(66));
 
 
 
